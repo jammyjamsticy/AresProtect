cf00f33b2e095443939edd4416ca298b17e429ff
jammyjamsticy <satishtripathy11@gmail.com>
2017-02-03 13:54:21 +0800

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -29,5 +29,14 @@ public class HelloWorld extends HttpServlet {
       obj.put("source", "java");
       out.print(obj);
     }
+	  public static void main(String[] args) throws Exception{
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new HelloWorld()),"/*");
+        server.start();
+        server.join();   
+    }
 
 }
