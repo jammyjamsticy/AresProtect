115eb8cd760bfeadd92171d01e1f024e7af55d78
jammyjamsticy <satishtripathy11@gmail.com>
2017-03-21 15:03:23 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -56,7 +56,7 @@ public class HelloWorld extends HttpServlet {
     // convert user object to json string and return it 
     String jsonStr= mapper.writeValueAsString(ojMessages);
     String jsonFormattedString = jsonStr.replaceAll("\\\\", "");
-		obj.put("messages",jsonFormattedString);
+	//	obj.put("messages",jsonFormattedString);
 	    System.out.println("jsonFormattedString :: "+jsonFormattedString);
 		out.print(obj);
     }
@@ -95,7 +95,7 @@ public class HelloWorld extends HttpServlet {
     String jsonStr= mapper.writeValueAsString(ojMessages);
      String jsonFormattedString = jsonStr.replaceAll("\\\\", "");
 
-		obj.put("messages",jsonFormattedString);
+	//	obj.put("messages",jsonFormattedString);
 		System.out.println("jsonFormattedString :: "+jsonFormattedString);
 		
       out.print(obj);
